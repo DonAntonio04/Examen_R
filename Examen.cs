3abb1658e9d3dcc3b32e83a3951a7f6cab88288c
ArturoZamoranoB <arturozamoranobeltran9@gmail.com>
2023-10-06 08:43:54 -0700

    Agregar archivos de proyecto.

---
+++ Examen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_R
+{
+    internal class Examen
+    {
+       public int Tabla(int z, int i)
+        {
+            
+            if( i < 11)
+            {
+                Console.WriteLine($" {i} x {z} = {i * z} ");
+            }
+
+            Tabla(z, i + 1);
+
+            return i;
+        }
+
+        public int Intervalo(int intervaloInferior , int intervaloSuperior)
+        {
+            if (intervaloInferior < intervaloSuperior)
+            {
+                for (int i = intervaloInferior; i < intervaloSuperior; i++) 
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            else 
+            {
+                for (int i = intervaloInferior; i > intervaloSuperior; i-- )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+          return intervaloInferior;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Examen_R
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la tabla de multiplicar deseada");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            
+
+            
+        }
+    }
+}
